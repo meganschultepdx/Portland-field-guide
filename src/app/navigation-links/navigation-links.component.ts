2a8ae6cb4971f622b76184c335b67ac68423aa24
AJ Ancheta <ashleyjancheta@gmail.com>
2019-06-24 16:11:26 -0700

    add initial component files for mission and navigation link page

---
+++ src/app/navigation-links/navigation-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-links',
+  templateUrl: './navigation-links.component.html',
+  styleUrls: ['./navigation-links.component.css']
+})
+export class NavigationLinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
