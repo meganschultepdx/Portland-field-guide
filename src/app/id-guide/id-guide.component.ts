5ce63af3ada91711535fb35a914933aae15c6b13
Student <example@example.com>
2019-06-26 11:23:35 -0700

    add collapsible function

--- src/app/id-guide/id-guide.component.ts
+++ src/app/id-guide/id-guide.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-id-guide',
@@ -10,6 +11,8 @@ export class IdGuideComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $('.collapsible').collapsible();
+    });
   }
-
 }
