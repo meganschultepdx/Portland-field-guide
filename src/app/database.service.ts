80772f73a7cf11bda3ad4c73ad30c87cae068055
Student <example@example.com>
2019-06-26 17:39:54 -0700

    add progress on search function

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { log } from 'util';
 
 @Injectable()
 export class DatabaseService {
   native_list: FirebaseListObservable<any[]>;
   invasive_list: FirebaseListObservable<any[]>;
   community_list: FirebaseListObservable<any[]>;
-  erradicate_list: FirebaseListObservable<any[]>;
+  // erradicate_list: AngularFireList<any[]>;
+  plants: any[] = [];
 
   constructor(private database: AngularFireDatabase) {
     this.native_list = database.list('native_plants/');
@@ -17,6 +19,50 @@ export class DatabaseService {
   getNativeList() {
     return this.native_list;
   }
+
+  getPlants() {
+    this.getInvasiveList().subscribe(values=> {
+      console.log(values);
+    });
+  }
+
+  getMustErradicate() {
+    this.getInvasiveList().subscribe(values=> {
+      for (var i = 0; i < values.length; i ++) {
+        if (values[i].is_erradication_req === true) {
+          this.plants.push(values[i]);
+        }
+      }
+      console.log(this.plants);
+      return this.plants;
+    });
+  }
+
+  getByRank(rank: string) {
+    this.getInvasiveList().subscribe(values=> {
+      for (var i = 0; i < values.length; i ++) {
+        if (values[i].invasive_rank === rank) {
+          this.plants.push(values[i]);
+        }
+      }
+      console.log(this.plants);
+      return this.plants;
+    });
+  }
+
+  getByRankAndErradicate(rank: string) {
+    this.getInvasiveList().subscribe(values=> {
+      for (var i = 0; i < values.length; i ++) {
+        if (values[i].invasive_rank === rank && values[i].is_erradication_req === true) {
+          this.plants.push(values[i]);
+        }
+      }
+      console.log(this.plants);
+      return this.plants;
+    });
+  }
+
+
 
   getInvasiveList() {
     return this.invasive_list;
@@ -30,11 +76,15 @@ export class DatabaseService {
   //   return this.database.object('flowers/' + flowerId);
   // }
 
-  getMustErradicate() {
-    return this.database.list('/invasive_plants', ref => ref.orderByChild('is_erradication_req').equalTo(true));
+  // test() {
+  //   // return this.invasive_list;
+  //   console.log(this.database.list('/invasive_plants').$ref.orderByChild('is_erradication_req').equalTo(true))
 
-  }
+  // }
 }
+
+// this.invasive_list.orderByChild('is_erradication_req').equalTo(true);
+// return this.database.list('/invasive_plants', ref => ref.orderByChild('is_erradication_req').equalTo(true));
 
 // let ref = database.list("invasive_plants");
 // ref("value", function(snapshot) {
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -13,14 +13,18 @@ declare var $: any;
 })
 export class SearchComponent implements OnInit {
   result_list: FirebaseListObservable<any[]>;
+  invasive_list: FirebaseListObservable<any[]>;
   searchOps: string = null;
-  results: string = null;
+  mustErradicate = false;
+  plants;
+
   constructor(private router: Router, private databaseService: DatabaseService) { }
 
   ngOnInit() {
-    // this.invasive_list = this.databaseService.getInvasiveList();
+    this.invasive_list = this.databaseService.getInvasiveList();
     // this.native_list = this.databaseService.getNativeList();
     // this.community_list = this.databaseService.getCommunityList();
+    this.plants = this.databaseService.getMustErradicate();
     $(document).ready(function(){
       $('select').formSelect();
     });
@@ -39,19 +43,41 @@ export class SearchComponent implements OnInit {
   }
 
   searchNative(nativeOrInvasive: string, byType: string, byCommunity: string, hasFlowers: string){
-    console.log("you clicked this button");
     if (nativeOrInvasive === 'native') {
-      this.results = 'native';
       this.result_list = this.databaseService.getNativeList();
-    }
+    } 
   }
 
   searchInvasive(nativeOrInvasive: string, erradicate: string, byInvasiveRank: string) {
-    console.log("you clicked this button");
-    if (nativeOrInvasive === 'invasive') {
-      this.results = 'invasive';
+    console.log(nativeOrInvasive);
+    console.log(erradicate);
+    console.log(byInvasiveRank);
+    if (nativeOrInvasive === 'invasive' && erradicate === "true" && byInvasiveRank === "A" || byInvasiveRank === "B" || byInvasiveRank === "C" || byInvasiveRank === "D" || byInvasiveRank === "W") {
+      this.result_list = null;
+      this.plants = this.databaseService.getByRank(byInvasiveRank);
+      console.log(this.plants);
+    } else if (nativeOrInvasive === 'invasive' && byInvasiveRank === "A" || byInvasiveRank === "B" || byInvasiveRank === "C" || byInvasiveRank === "D" || byInvasiveRank === "W") {
+      this.result_list = null;
+      this.plants = this.databaseService.getByRankAndErradicate(byInvasiveRank);
+      console.log(this.plants);
+    } else if (nativeOrInvasive === 'invasive' && erradicate === "true") {
+      this.result_list = null;
+      this.mustErradicate = true;
+      // this.databaseService.test();
+      console.log(this.plants);
+      console.log(this.invasive_list);
+    } else if (nativeOrInvasive === 'invasive') {
+      this.plants = null;
       this.result_list = this.databaseService.getInvasiveList();
     }
   }
 
 }
+
+
+
+// (value => {
+//   console.log(value);
+  
+//   value.is_erradication_req === true;
+// })
