57d33fbe982db2bdd65e383c523f9cb8b5c7967f
Student <example@example.com>
2019-06-26 17:32:55 -0700

    add picture files

---
+++ src/app/glossary/glossary.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+declare var $: any;
+
+@Component({
+  selector: 'app-glossary',
+  templateUrl: './glossary.component.html',
+  styleUrls: ['./glossary.component.css'],
+  // selector: 'ng-if-simple',
+
+})
+
+export class GlossaryComponent implements OnInit {
+
+  constructor() { }
+
+  alphabetLetter = null;
+
+  showDiv(input: string) {
+    this.alphabetLetter = input;
+  }
+
+  ngOnInit() {
+    $(document).ready(function(){
+      $('.collapsible').collapsible();
+    });
+  }
+
+}
